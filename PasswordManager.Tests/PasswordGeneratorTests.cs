c53cabb40f12c25e911c2842d1f511a5ee77b515
Dominic Maas <dominic.maas@live.com>
2021-06-06 20:01:31 +1200

    password generation

---
+++ PasswordManager.Tests/PasswordGeneratorTests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PasswordManager.Tests
+{
+    public class PasswordGeneratorTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public PasswordGeneratorTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void LengthTooSmall()
+        {
+            using var passwordGenerator = new PasswordGenerator();
+            Assert.Throws<ArgumentException>(() => passwordGenerator.GeneratePassword(5));
+        }
+        
+        [Fact]
+        public void LengthTooLong()
+        {
+            using var passwordGenerator = new PasswordGenerator();
+            Assert.Throws<ArgumentException>(() => passwordGenerator.GeneratePassword(100));
+        }
+
+        [Fact]
+        public void SingleGeneration()
+        {
+            using var passwordGenerator = new PasswordGenerator();
+            passwordGenerator.GeneratePassword(80);
+        }
+        
+        [Fact]
+        public void MassGeneration()
+        {
+            // Keep track of counts to see distribution
+            var dict = new Dictionary<char, int>();
+            
+            using var passwordGenerator = new PasswordGenerator();
+            for (var i = 0; i < 50_000_000; i++) // 50 million passwords
+            {
+                var pass = passwordGenerator.GeneratePassword(80);
+                foreach (var c in pass.ToCharArray())
+                {
+                    if (dict.ContainsKey(c))
+                    {
+                        dict[c] += 1;
+                    }
+                    else
+                    {
+                        dict.Add(c, 1);
+                    }
+                }
+            }
+
+            foreach (var (key, value) in dict.OrderBy(x => x.Key))
+            {
+                _outputHelper.WriteLine($"{key} : {value}");
+            }
+        }
+    }
+}
---
+++ PasswordManager/PasswordGenerator.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Security.Cryptography;
+
+namespace PasswordManager
+{
+    /// <summary>
+    ///     The password generator allows the creation of cryptographically secure
+    ///     random passwords between 8 and 80 characters.
+    /// </summary>
+    public class PasswordGenerator : IDisposable
+    {
+        // Cryptographically secure random number generation
+        private readonly RNGCryptoServiceProvider _cryptoServiceProvider;
+
+        // These are valid characters that may be used in the password
+        private const string ValidPasswordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_+=[]{};':<>?,./|\"\\~`";
+
+        /// <summary>
+        ///     Create a new instance of 'PasswordGenerator'
+        /// </summary>
+        public PasswordGenerator()
+        {
+            _cryptoServiceProvider = new RNGCryptoServiceProvider();
+        }
+
+        public string GeneratePassword(int length)
+        {
+            // Guards
+            if (length < 8) throw new ArgumentException("Password length must be at least 8 characters long", nameof(length));
+            if (length > 80) throw new ArgumentException("Password length must be less than or equal to 80 characters long", nameof(length));
+            
+            // Converting to a char array for easy access
+            var potentialPasswordChars = ValidPasswordCharacters.ToCharArray();
+            
+            var generatedResult = new char[length];
+            
+            var bytes = new byte[length * 8];
+            _cryptoServiceProvider.GetBytes(bytes);
+            
+            // There is a small bias due to the modulus operation not spreading the entire width of ulong equally into {NUMBER} chars. 
+            for (var i = 0; i < length; i++)
+            {
+                var value = BitConverter.ToUInt64(bytes, i * 8);
+                generatedResult[i] = potentialPasswordChars[value % (uint)potentialPasswordChars.Length];
+            }
+
+            return new string(generatedResult);
+        }
+        
+        /// <summary>
+        ///     Dispose resources 
+        /// </summary>
+        public void Dispose()
+        {
+            _cryptoServiceProvider?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
