91aa64c90c95354cf523e983f89569f3edca5c14
Dominic Maas <dominic.maas@live.com>
2021-06-08 09:55:59 +1200

    data encryptor

--- PasswordManager.Tests/IntegrationTests.cs
+++ PasswordManager.Tests/IntegrationTests.cs
@@ -16,5 +16,52 @@ namespace PasswordManager.Tests
             Assert.Equal(256, hashedPassword.Hash.Length);
             Assert.Equal(32, hashedPassword.Salt.Length);
         }
+
+        [Fact]
+        public void TestGenerateAndHashAndEncrypt()
+        {
+            using var passwordGenerator = new PasswordGenerator();
+            using var passwordHasher = new PasswordHasher();
+            using var dataEncryptor = new DataEncryptor();
+            
+            var data = new byte[] {0x00, 0x08, 0x00, 0x00, 0x08, 0x03, 0x08, 0x03, 0x08, 0x03};
+            
+            var randomPassword = passwordGenerator.GeneratePassword(80);
+            var key = passwordHasher.HashPassword(randomPassword, 256 / 8);
+            var encryptedData = dataEncryptor.Encrypt(key.Hash, data);
+            
+            Assert.NotEqual(data, encryptedData.Data);
+        }
+        
+        [Fact]
+        public void TestGenerateAndHashAndEncryptAndDecrypt()
+        {
+            using var passwordGenerator = new PasswordGenerator();
+            using var passwordHasher = new PasswordHasher();
+            using var dataEncryptor = new DataEncryptor();
+            
+            var data = new byte[] {0x00, 0x08, 0x00, 0x00, 0x08, 0x03, 0x08, 0x03, 0x08, 0x03};
+            
+            var randomPassword = passwordGenerator.GeneratePassword(80);
+            var key = passwordHasher.HashPassword(randomPassword, 256 / 8);
+            var encryptedData = dataEncryptor.Encrypt(key.Hash, data);
+            
+            // ------ Stored to file ------ //
+            var cipherText = encryptedData.Data;
+            var salt = key.Salt;
+            var nounce = encryptedData.Nounce;
+            var tag = encryptedData.Tag;
+            // ------ Stored to file ------ //
+            
+            var newKey = passwordHasher.HashPassword(randomPassword, salt, 256 / 8); // User re-enters password, use stored salt
+           
+            Assert.Equal(key.Hash, newKey.Hash);
+            Assert.Equal(key.Salt, newKey.Salt);
+
+            var plainText = dataEncryptor.Decrypt(newKey.Hash,
+                new DataEncryptor.EncryptedData {Data = cipherText, Nounce = nounce, Tag = tag});
+            
+            Assert.Equal(data, plainText);
+        }
     }
 }
---
+++ PasswordManager/Common/DataEncryptor.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Security.Cryptography;
+
+namespace PasswordManager.Common
+{
+    public class DataEncryptor : IDisposable
+    {
+        // Cryptographically secure random number generation
+        private readonly RNGCryptoServiceProvider _cryptoServiceProvider;
+        
+        /// <summary>
+        ///     Create a new instance of 'DataEncryptor'
+        /// </summary>
+        public DataEncryptor()
+        {
+            _cryptoServiceProvider = new RNGCryptoServiceProvider();
+        }
+        
+        public EncryptedData Encrypt(byte[] key, byte[] data)
+        {
+            // Use AES in GCM mode as per 
+            // https://cheatsheetseries.owasp.org/cheatsheets/Cryptographic_Storage_Cheat_Sheet.html#cipher-modes
+            using var aes = new AesGcm(key);
+
+            // Generate the nounce (maximum possible size)
+            var nounce = new byte[AesGcm.NonceByteSizes.MaxSize];
+            _cryptoServiceProvider.GetBytes(nounce);
+            
+            var encryptedData = new byte[data.Length];
+            var tag = new byte[AesGcm.TagByteSizes.MaxSize]; // use large tag sizes!
+            aes.Encrypt(nounce, data, encryptedData, tag);
+
+            return new EncryptedData
+            {
+                Data = encryptedData,
+                Nounce = nounce,
+                Tag = tag
+            };
+        }
+
+        public byte[] Decrypt(byte[] key, EncryptedData data)
+        {
+            // Use AES in GCM mode as per 
+            // https://cheatsheetseries.owasp.org/cheatsheets/Cryptographic_Storage_Cheat_Sheet.html#cipher-modes
+            using var aes = new AesGcm(key);
+
+            var plainText = new byte[data.Data.Length];
+            aes.Decrypt(data.Nounce, data.Data, data.Tag, plainText);
+
+            return plainText;
+        }
+        
+        public void Dispose()
+        {
+            _cryptoServiceProvider.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        
+        public struct EncryptedData
+        {
+            public byte[] Data;
+            public byte[] Nounce;
+            public byte[] Tag;
+        }
+    }
+}
